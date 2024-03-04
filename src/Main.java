74c79eff9b369d44e074ef833b110d023bb4a2b0
Mark <marksmunari@gmail.com>
2024-03-04 15:19:22 -0300

    add input from keyboard in Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import check.word.WordChecker;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println(WordChecker.checkWord(scanner.next()));
+
+        scanner.close();
     }
 }
