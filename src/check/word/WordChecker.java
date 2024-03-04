302b9dbeb1cfbc459fbaad8c2d2d204fdd9b4e7a
Mark <marksmunari@gmail.com>
2024-03-04 14:38:17 -0300

    add checkWord method implementation

--- src/check/word/WordChecker.java
+++ src/check/word/WordChecker.java
@@ -2,6 +2,18 @@ package check.word;
 
 public class WordChecker {
     public static String checkWord(String word) {
-        return "sanduiche";
+        for (int i = 0; i < word.length(); i++) {
+            if(((word.length() - i) % 2) != 0) continue;
+
+            final String substringToCompare = word.substring(i, word.length());
+            final String wordInitToCompare = substringToCompare.substring(0, (substringToCompare.length() / 2));
+            final String wordEndToCompare = substringToCompare.substring(substringToCompare.length() / 2);
+
+            if (wordInitToCompare.compareTo(wordEndToCompare) == 0) {
+                return word.substring(0, (word.length() - wordEndToCompare.length()));
+            }
+        }
+
+        return word;
     }
 }
