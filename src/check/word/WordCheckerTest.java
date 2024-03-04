b9136b83d10bc9c55921607e1b8079a4cc52df0b
Mark <marksmunari@gmail.com>
2024-03-04 15:14:46 -0300

    add teste for others words

--- src/check/word/WordCheckerTest.java
+++ src/check/word/WordCheckerTest.java
@@ -9,11 +9,11 @@ class WordCheckerTest {
     @Test
     void wordCheckerShouldRemoveDuplicityInSanduicheWord() {
         // Given:
-        final String sanduiche = "sanduicheiche";
+        final String incorrectWord = "sanduicheiche";
         final String expectedResult = "sanduiche";
 
         // When:
-        final String result = WordChecker.checkWord(sanduiche);
+        final String result = WordChecker.checkWord(incorrectWord);
 
         // Then:
         assertEquals(expectedResult, result);
@@ -22,13 +22,92 @@ class WordCheckerTest {
     @Test
     void wordCheckerShouldRemoveDuplicityInBarrilWord() {
         // Given:
-        final String sanduiche = "barrilarril";
+        final String incorrectWord = "barrilarril";
         final String expectedResult = "barril";
 
         // When:
-        final String result = WordChecker.checkWord(sanduiche);
+        final String result = WordChecker.checkWord(incorrectWord);
 
         // Then:
         assertEquals(expectedResult, result);
     }
+
+    @Test
+    void wordCheckerShouldRemoveDuplicityInRatoWord() {
+        // Given:
+        final String incorrectWord = "ratoato";
+        final String expectedResult = "rato";
+
+        // When:
+        final String result = WordChecker.checkWord(incorrectWord);
+
+        // Then:
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void wordCheckerShouldRemoveDuplicityInSolWord() {
+        // Given:
+        final String incorrectWord = "sol";
+        final String expectedResult = "sol";
+
+        // When:
+        final String result = WordChecker.checkWord(incorrectWord);
+
+        // Then:
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void wordCheckerShouldRemoveDuplicityInColiseuWord() {
+        // Given:
+        final String incorrectWord = "coliseueu";
+        final String expectedResult = "coliseu";
+
+        // When:
+        final String result = WordChecker.checkWord(incorrectWord);
+
+        // Then:
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void wordCheckerShouldRemoveDuplicityInQueijoWord() {
+        // Given:
+        final String incorrectWord = "queijoijo";
+        final String expectedResult = "queijo";
+
+        // When:
+        final String result = WordChecker.checkWord(incorrectWord);
+
+        // Then:
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void wordCheckerShouldRemoveDuplicityInAstroWord() {
+        // Given:
+        final String incorrectWord = "astroastro";
+        final String expectedResult = "astro";
+
+        // When:
+        final String result = WordChecker.checkWord(incorrectWord);
+
+        // Then:
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void wordCheckerShouldRemoveDuplicityInAChar() {
+        // Given:
+        final String incorrectChar = "a";
+        final String expectedResult = "a";
+
+        // When:
+        final String result = WordChecker.checkWord(incorrectChar);
+
+        // Then:
+        assertEquals(expectedResult, result);
+    }
+
 }
